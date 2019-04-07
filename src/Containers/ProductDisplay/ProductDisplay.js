f976a12eaaed4034a3998ada4517e032b9345696
Cancain <eriksson.tomas@gmail.com>
2019-04-07 17:40:15 +0200

    added description and prive to productDisplay

--- src/Containers/ProductDisplay/ProductDisplay.js
+++ src/Containers/ProductDisplay/ProductDisplay.js
@@ -4,10 +4,14 @@ import { clones } from "../../MockData/Products";
 
 const productDisplay = props => {
   const product = new URLSearchParams(props.location.search);
+  const id = product.get("id");
+  const subId = product.get("subId");
 
   return (
     <div>
-      <h1>{clones[product.get("id")].subItems[product.get("subId")].name}</h1>
+      <h1>{clones[id].subItems[subId].name}</h1>
+      <strong>Pris: {clones[id].subItems[subId].price} SEK</strong>
+      <p>{clones[id].subItems[subId].desc}</p>
     </div>
   );
 };
--- src/MockData/Products.js
+++ src/MockData/Products.js
@@ -1,6 +1,28 @@
 const chiliSubItems = [
-  { id: 0, name: "Basket of fire", price: 30, parentId: 0 },
-  { id: 1, name: "Jalapeño", price: 32, parentId: 0 }
+  {
+    id: 0,
+    name: "Basket of fire",
+    price: 30,
+    parentId: 0,
+    desc:
+      "Basket of Fire F1 är en underbart produktiv och dekorativ Chili. Det unika, buskiga växtsättet som Chilli Peppar ‘Basket of Fire’ har gör den perfekt för amplar och krukor. Denna färggranna sort blir översållad med små, mycket heta chilifrukter som mognar från mörkt purpurröda till gula och orange till en ljus röd nyans, fantastisk dekorativ. Plantorna visar god hållbarhet i svalt väder och fortsätter att sätta frukt utomhus långt in på hösten och ännu längre under glas. Dessa kraftfulla chilis har en Scoville styrka på cirka 80000 shu, används färska eller torkade."
+  },
+  {
+    id: 1,
+    name: "Jalapeño",
+    price: 32,
+    parentId: 0,
+    desc:
+      "Mexikansk chilipeppar. Kompakta plantor med 6 cm avlånga, svagt koniska, lite knubbiga, gröna frukter som vid full mognad övergår i rött. Styrka: mild."
+  },
+  {
+    id: 2,
+    name: "De Cayenne",
+    price: 15,
+    parentId: 0,
+    desc:
+      "Klassisk chilipeppar. Långsmala, gröna frukter som vid full mognad övergår i rött. Ca 20-25 gånger starkare än vanlig paprika. Styrka: het."
+  }
 ];
 
 export const clones = [
